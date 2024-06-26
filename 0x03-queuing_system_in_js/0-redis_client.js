012458e2d7d7aaa13a6fe17d9ac573716ee0395c
Lance Desk <lncedesk@gmail.com>
2024-06-26 06:36:37 -0700

    Node Redis Client.

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,17 @@
+import { createClient } from 'redis';
+
+// Create a Redis client
+const client = createClient();
+
+// Handle successful connection
+client.on('connect', () => {
+    console.log('Redis client connected to the server');
+});
+
+// Handle connection errors
+client.on('error', (err) => {
+    console.log('Redis client not connected to the server:', err.message);
+});
+
+// Connect the client
+client.connect();
